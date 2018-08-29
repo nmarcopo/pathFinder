dafe871941c36e4daa51dd40d80735eb77cf2d55
nmarcopo <nick@marcopo.li>
2018-08-29 13:28:03 +0000

    finally fixed issue where pathFinder runs from ip address but not nick.marcopo.li

--- src/python-server.py
+++ src/python-server.py
@@ -29,34 +29,35 @@ class ForkingHTTPServer(ForkingMixIn, HTTPServer):
 class WWWHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == '/':
-            self.path = '/index.html'
+	if self.path == '/':
+	    self.path = '/index.html'
 
-        mimetype = determine_mimetype(self.path)
-        realpath = os.path.abspath(WWW_ROOT + self.path)
+	mimetype = determine_mimetype(self.path)
+	realpath = os.path.abspath(WWW_ROOT + self.path)
 
-        if not realpath.startswith(WWW_ROOT):
-            self.send_error(404, 'Invalid file path: {}'.format(self.path))
-            return
+	if not realpath.startswith(WWW_ROOT):
+	    self.send_error(404, 'Invalid file path: {}'.format(self.path))
+	    return
 
-        try:
-            with open(realpath, 'rb') as fh:
-                self.send_response(200)
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                self.wfile.write(fh.read())
-        except IOError as e:
-            self.send_error(404, 'File Not Found: {}\n{}'.format(self.path, e))
+	try:
+	    with open(realpath, 'rb') as fh:
+		self.send_response(200)
+		self.send_header('Content-type', mimetype)
+		self.end_headers()
+		self.wfile.write(fh.read())
+	except IOError as e:
+	    self.send_error(404, 'File Not Found: {}\n{}'.format(self.path, e))
 
     def do_POST(self):
-        data = self.rfile.read(int(self.headers.getheader('content-length')))
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+	data = self.rfile.read(int(self.headers.getheader('content-length')))
+	self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+	self.send_header('Content-type', 'application/json')
+	self.end_headers()
 
-        p = subprocess.Popen(['./dijkstras'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        d = [map(int, line.split()) for line in p.communicate(data)[0].splitlines()]
-        json.dump({'path': d[1:], 'cost': d[0]}, self.wfile)
+	p = subprocess.Popen(['./dijkstras'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	d = [map(int, line.split()) for line in p.communicate(data)[0].splitlines()]
+	json.dump({'path': d[1:], 'cost': d[0]}, self.wfile)
 
 # Usage
 
@@ -68,20 +69,20 @@ def usage(exit_code):
 
 if __name__ == '__main__':
     try:
-        options, arguments = getopt.getopt(sys.argv[1:], "hd:p:")
+	options, arguments = getopt.getopt(sys.argv[1:], "hd:p:")
     except getopt.GetoptError as e:
-        usage(1)
+	usage(1)
 
     for option, value in options:
-        if option == '-h':
-            usage(0)
-        elif option == '-d':
-            WWW_ROOT = value
-        elif option == '-p':
-            WWW_PORT = int(value)
-        else:
-            usage(1)
-    
+	if option == '-h':
+	    usage(0)
+	elif option == '-d':
+	    WWW_ROOT = value
+	elif option == '-p':
+	    WWW_PORT = int(value)
+	else:
+	    usage(1)
+
     server = ForkingHTTPServer(('', WWW_PORT), WWWHandler)
     print 'Started HTTP Server on port', WWW_PORT
     server.serve_forever()
